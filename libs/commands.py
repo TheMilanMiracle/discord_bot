b11f90bd52d2e09ba372d040991d21a27bbb57c9
TheMilanMiracle <lxziano.mc@gmail.com>
2023-09-10 23:21:14 -0300

    minor changes in commands.py

--- libs/commands.py
+++ libs/commands.py
@@ -17,6 +17,11 @@ async def operate_command(interaction:discord.Interaction):
 async def avatar_command(interaction:discord.Interaction, member:discord.Member):
     await interaction.response.send_message(member.display_avatar)
     
-@botClient.tree.command(name="icon", description="show the icon of the sender of the command")
-async def icon_command(interaction:discord.Interaction, member:discord.Member):
-    await interaction.response.send_message(member.display_icon)
+
+@botClient.tree.context_menu(name="roulette1")
+async def roulette_command1(interaction: discord.Interaction, message: discord.Message):
+    await interaction.response.send_message("test", ephemeral=True)
+    
+@botClient.tree.command(name="roulette2", description="Rolls the roullete. Bet your points here!")
+async def roulette_command2(interaction: discord.Interaction, message: str):
+    await interaction.response.send_message(f"Your message was {message}", ephemeral=True)
